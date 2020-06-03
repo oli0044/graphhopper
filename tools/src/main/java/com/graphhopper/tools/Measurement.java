96d310eb284475eef3e41519623a63c77ac3461b
easbar <easbar.mail@posteo.net>
2020-06-03 15:41:47 +0200

    Make it less extreme

--- tools/src/main/java/com/graphhopper/tools/Measurement.java
+++ tools/src/main/java/com/graphhopper/tools/Measurement.java
@@ -479,7 +479,7 @@ public class Measurement {
             print("unit_testsCH.get_weight", miniPerf);
 
             gcAndWait();
-            int countForNew = count * 100;
+            int countForNew = count / 100;
             RoutingCHGraphImpl routingCHGraph = new RoutingCHGraphImpl(lg, chWeighting);
             final RoutingCHEdgeExplorer chOutEdgeExplorer = routingCHGraph.createOutEdgeExplorer();
             miniPerf = new MiniPerfTest() {
